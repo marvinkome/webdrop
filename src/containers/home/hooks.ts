05f4d06151abea922d127386d0948c3cc3649681
Marvin Kome <marvinkome@gmail.com>
2021-03-06 18:40:40 +0100

    add listeners

--- src/containers/home/hooks.ts
+++ src/containers/home/hooks.ts
@@ -123,12 +123,14 @@ export function useTransfer(peerConn?: Peer) {
 
     useEffect(() => {
         if (!peerConn) {
-            console.log("[connectionListener] no connection to sender")
+            console.log("[connectionListener] peerConn not yet available")
             return
+        } else {
+            console.log("[connectionListener] peerConn available")
         }
 
         peerConn.on("connection", (conn) => {
-            setTransferType("upload")
+            setTransferType("download")
             dataConn.current = conn
 
             conn.on("open", () => {
